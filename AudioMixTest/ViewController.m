127733c7d30a70c097bbc181461f2f723380e687
Sajiv Nair <Sajiv@Rameshs-Macintosh.local>
2015-05-18 13:35:36 +0530

    Added loop for audiotracks

--- AudioMixTest/ViewController.m
+++ AudioMixTest/ViewController.m
@@ -32,86 +32,54 @@
 
 - (NSString*)mixAudioFiles:(NSArray*)audioFileURLArray {
     
-    NSString *masterFile;
-    NSString *activeFile;
-    AVURLAsset* masterAsset;
-    AVURLAsset* activeAsset;
-    
     NSError* error = nil;
+    AVURLAsset* fileAsset;
+    NSString *outputFile;
     AVAssetExportSession* exportSession;
-    
+    AVMutableCompositionTrack* audioTrack;
     AVMutableComposition* composition = [AVMutableComposition composition];
-    AVMutableCompositionTrack* audioTrack1 = [composition addMutableTrackWithMediaType:AVMediaTypeAudio
-                                                                     preferredTrackID:kCMPersistentTrackID_Invalid];
-    AVMutableCompositionTrack* audioTrack2 = [composition addMutableTrackWithMediaType:AVMediaTypeAudio
-                                                                      preferredTrackID:kCMPersistentTrackID_Invalid];
-    int size = [audioFileURLArray count];
-    NSFileManager *fileManager = [NSFileManager defaultManager];
     
-    for(int i = 1; i < size; i++) {
+    int size = [audioFileURLArray count];
+    
+    for(int i = 0; i < size; i++) {
         
-        if(i == 1) {
-            masterFile = [audioFileURLArray objectAtIndex:(i - 1)];
-        }
+        audioTrack = [composition addMutableTrackWithMediaType:AVMediaTypeAudio
+                                  preferredTrackID:kCMPersistentTrackID_Invalid];
         
-        activeFile = [audioFileURLArray objectAtIndex:i];
+        fileAsset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:[audioFileURLArray objectAtIndex:i]]      options:nil];
         
-        // grab the two audio assets as AVURLAssets according to the file paths
-        masterAsset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:masterFile] options:nil];
-        activeAsset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:activeFile] options:nil];
-        
-        [audioTrack1 insertTimeRange:CMTimeRangeMake(kCMTimeZero, masterAsset.duration)
-                     ofTrack:[[masterAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
-                     atTime:kCMTimeZero
-                     error:&error];
-        
-        // mix the entirety of the active recording
-        [audioTrack2 insertTimeRange:CMTimeRangeMake(kCMTimeZero, activeAsset.duration)
-                     ofTrack:[[activeAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
-                     atTime:kCMTimeZero
-                     error:&error];
-        
-        // now export the two files
-        // create the export session
-        // no need for a retain here, the session will be retained by the
-        // completion handler since it is referenced there
-        
-        exportSession = [AVAssetExportSession
-                         exportSessionWithAsset:composition
-                         presetName:AVAssetExportPresetAppleM4A];
-        
-        if((i > 1) && [fileManager fileExistsAtPath:masterFile]) {
-            [fileManager removeItemAtPath:masterFile error:&error];
-        }
-
-        // create a new file for the combined file
-        masterFile = @"/Users/Sajiv/Downloads/Combined.m4a";
-        
-        // configure export session  output with all our parameters
-        exportSession.outputURL = [NSURL fileURLWithPath:masterFile]; // output path
-        exportSession.outputFileType = AVFileTypeAppleM4A; // output file type
-        
-        [exportSession exportAsynchronouslyWithCompletionHandler:^{
-            
-            // export status changed, check to see if it's done, errored, waiting, etc
-            switch (exportSession.status)
-            {
-                case AVAssetExportSessionStatusFailed:
-                    break;
-                case AVAssetExportSessionStatusCompleted:
-                    break;
-                case AVAssetExportSessionStatusWaiting:
-                    break;
-                default:
-                    break;
-            }
-        }];
-        
-        // Give some time for mixed file creation.
-        sleep(5);
+        [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, fileAsset.duration)
+                             ofTrack:[[fileAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
+                             atTime:kCMTimeZero
+                             error:&error];
     }
     
-    return masterFile;
+    exportSession = [AVAssetExportSession
+                     exportSessionWithAsset:composition
+                     presetName:AVAssetExportPresetAppleM4A];
+    
+    outputFile = @"/Users/Sajiv/Downloads/Combined.m4a";
+    
+    exportSession.outputURL = [NSURL fileURLWithPath:outputFile]; // output path
+    exportSession.outputFileType = AVFileTypeAppleM4A; // output file type
+    
+    [exportSession exportAsynchronouslyWithCompletionHandler:^{
+        
+        // export status changed, check to see if it's done, errored, waiting, etc
+        switch (exportSession.status)
+        {
+            case AVAssetExportSessionStatusFailed:
+                break;
+            case AVAssetExportSessionStatusCompleted:
+                break;
+            case AVAssetExportSessionStatusWaiting:
+                break;
+            default:
+                break;
+        }
+    }];
+    
+    return outputFile;
 }
 
 @end
